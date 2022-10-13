c97d567291e72045e3c68ed363204a0f19226098
Harry <harrywendson@gmail.com>
2022-10-12 22:57:15 -0300

    Entidade Product

---
+++ src/main/java/com/webservices/projetoweb/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.webservices.projetoweb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webservices.projetoweb.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
