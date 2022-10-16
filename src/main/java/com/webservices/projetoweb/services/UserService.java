c92cac4dc2b6ce5f995fe9cb33b11118068c8f69
Harry <harrywendson@gmail.com>
2022-10-15 23:17:54 -0300

    Tratamento de execeção - findById

--- src/main/java/com/webservices/projetoweb/services/UserService.java
+++ src/main/java/com/webservices/projetoweb/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.webservices.projetoweb.entities.User;
 import com.webservices.projetoweb.repositories.UserRepository;
+import com.webservices.projetoweb.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
