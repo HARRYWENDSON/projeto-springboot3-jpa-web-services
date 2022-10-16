6c57fc75effb0f0da4c42990d0f466118e20e235
Harry <harrywendson@gmail.com>
2022-10-15 21:29:31 -0300

    Entidade Payment, associação um para um

--- src/main/java/com/webservices/projetoweb/config/TestConfig.java
+++ src/main/java/com/webservices/projetoweb/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.webservices.projetoweb.entities.Category;
 import com.webservices.projetoweb.entities.Order;
 import com.webservices.projetoweb.entities.OrderItem;
+import com.webservices.projetoweb.entities.Payment;
 import com.webservices.projetoweb.entities.Product;
 import com.webservices.projetoweb.entities.User;
 import com.webservices.projetoweb.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
