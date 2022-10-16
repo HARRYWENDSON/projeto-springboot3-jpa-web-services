87354b2ca42b9651d902598b9c9296795c160f16
Harry <harrywendson@gmail.com>
2022-10-15 21:41:53 -0300

    Métodos sudTotal e total

--- src/main/java/com/webservices/projetoweb/entities/OrderItem.java
+++ src/main/java/com/webservices/projetoweb/entities/OrderItem.java
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSudTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
